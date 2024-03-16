b26ad4c72462c73772fd8da6bf8dd05fcf5741e0
JordyNtambwe <jordyntambwe@gmail.com>
2024-03-15 19:57:09 -0600

    commit on master

--- fetcher.js
+++ fetcher.js
@@ -3,16 +3,15 @@ const request = require("request")
 const url = process.argv[2]
 const filePath = process.argv[3]
 
-request(url, (error, response, body) => {
+request(`${url}`, (error, response, body) => {
   if(error) {
     console.log("Error:", error)
   }
-fs.writeFile(filePath, body, (error) => {
+fs.writeFile(`${filePath}`, body, (error) => {
   if (error) {
     console.log("Error", error)
   } else {
     console.log(`Downloaded and saved ${body.length} bytes to ${filePath}`)
   }
+ })
 })
-})
-
